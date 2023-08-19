bf5f8bdbdf662c9b4995b958417cee11468605fe
HeewonP825 <7hwon@naver.com>
2023-08-20 03:53:18 +0900

    Design(fragment_all_meeting.xml): add AllMeetingFragmentxml

--- app/src/main/java/com/daon/daon_ui/ui/allMeeting/AllMeetingFragment.kt
+++ app/src/main/java/com/daon/daon_ui/ui/allMeeting/AllMeetingFragment.kt
@@ -1,13 +1,22 @@
 package com.daon.daon_ui.ui.allMeeting
 
+import android.annotation.SuppressLint
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.daon.daon_ui.R
 import com.daon.daon_ui.databinding.FragmentAllMeetingBinding
+import com.google.android.material.tabs.TabLayout
 
 class AllMeetingFragment : Fragment() {
 
@@ -32,8 +41,120 @@ class AllMeetingFragment : Fragment() {
 //        dashboardViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        // Spinner 1 설정
+        val spinner1 = binding.sortCondition1Spinner
+        val sortCondition1Options = arrayOf("전체", "Option 2", "Option 3")
+        val spinnerAdapter1 = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sortCondition1Options)
+        spinnerAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = spinnerAdapter1
+
+        // Spinner 2 설정
+        val spinner2 = binding.sortCondition2Spinner
+        val sortCondition2Options = arrayOf("날짜순", "Option B", "Option C")
+        val spinnerAdapter2 = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sortCondition2Options)
+        spinnerAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.adapter = spinnerAdapter2
+
+        // Spinner 아이템 선택 리스너
+        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedOption = sortCondition1Options[position]
+                // 선택된 옵션에 따른 동작 수행
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // 아무것도 선택되지 않았을 때의 동작
+            }
+        }
+
+        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedOption = sortCondition2Options[position]
+                // 선택된 옵션에 따른 동작 수행
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // 아무것도 선택되지 않았을 때의 동작
+            }
+        }
+
+        val tabLayout = root.findViewById<TabLayout>(R.id.tabLayout)
+
+        val tab1 = tabLayout.newTab()
+        tab1.customView = createTabView("내주변 (48)", R.drawable.ic_all_meeting_location)
+        tabLayout.addTab(tab1)
+
+        val tab2 = tabLayout.newTab()
+        tab2.customView = createTabView("전국 (32)", R.drawable.ic_all_meeting_location)
+        tabLayout.addTab(tab2)
+
+        val tab3 = tabLayout.newTab()
+        tab3.customView = createTabView("서울 (13)", R.drawable.ic_all_meeting_location)
+        tabLayout.addTab(tab3)
+
+        val tab4 = tabLayout.newTab()
+        tab4.customView = createTabView("경기 (9)", R.drawable.ic_all_meeting_location)
+        tabLayout.addTab(tab4)
+
+        val tab5 = tabLayout.newTab()
+        tab5.customView = createTabView("충청 (4)", R.drawable.ic_all_meeting_location)
+        tabLayout.addTab(tab5)
+
+        tabLayout.selectTab(tab1)
+
+        // 아이콘을 숨길 나머지 탭에 대한 로직 추가
+        for (i in 1 until tabLayout.tabCount) {
+            val tab = tabLayout.getTabAt(i)
+            val customView = tab?.customView
+            val iconView = customView?.findViewById<ImageView>(R.id.tabIcon)
+
+            iconView?.visibility = View.GONE
+        }
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val customView = tab.customView
+                val textView = customView?.findViewById<TextView>(R.id.tabText)
+                val iconView = customView?.findViewById<ImageView>(R.id.tabIcon)
+
+                textView?.setTextColor(ContextCompat.getColor(requireActivity(), R.color.mainColor))
+                textView?.typeface = Typeface.DEFAULT_BOLD // 선택된 탭의 폰트를 굵게 변경
+                iconView?.visibility = View.VISIBLE
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                val customView = tab.customView
+                val iconView = customView?.findViewById<ImageView>(R.id.tabIcon)
+                val textView = customView?.findViewById<TextView>(R.id.tabText)
+
+                textView?.setTextColor(ContextCompat.getColor(requireActivity(), R.color.tabbar_title))
+                textView?.typeface = Typeface.DEFAULT // 선택된 탭의 폰트를 굵게 변경
+                iconView?.visibility = View.GONE
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                // 선택된 탭을 다시 선택했을 때의 동작
+            }
+        })
+
         return root
     }
+
+    @SuppressLint("MissingInflatedId")
+    private fun createTabView(text: String, iconResId: Int): View {
+        val tabView = layoutInflater.inflate(R.layout.custom_tab_layout, null)
+
+        val textView = tabView.findViewById<TextView>(R.id.tabText)
+        val iconView = tabView.findViewById<ImageView>(R.id.tabIcon)
+
+        textView.text = text
+        iconView.setImageResource(iconResId)
+        iconView.visibility = View.GONE // 처음에는 아이콘을 숨김
+
+        return tabView
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
